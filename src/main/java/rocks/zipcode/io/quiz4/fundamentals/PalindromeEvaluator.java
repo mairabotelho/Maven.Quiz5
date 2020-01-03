bd5989524490dff29637bd997e391b3aadd5621d
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-4.local>
2020-01-03 10:23:23 -0500

    fundamentals

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,38 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i <= string.length(); i++) {
+            for (int j = i; j < string.length(); j++){
+                if (isPalindrome(string.substring(i, j + 1))){
+                    if(!(result.contains(string.substring(i, j + 1)))){
+                        result.add(string.substring(i, j + 1));
+                    }
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+
+        if(string.equals(reverseString(string)))
+            return true;
+
+        return false;
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder str = new StringBuilder(string);
+
+        return str.reverse().toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,18 +1,54 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i <= string.length(); i++) {
+            for (int j = i; j < string.length(); j++){
+                    if(!result.contains(string.substring(i, j + 1))){
+                        result.add(string.substring(i, j + 1));
+                    }
+                }
+            }
+
+        return result.toArray(new String[0]);
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        String[] str1 = getAllSubstrings(string1);
+        String[] str2 = getAllSubstrings(string2);
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < str1.length; i++){
+            for(int j = 0; j < str2.length; j++){
+
+                if(str1[i].equals(str2[j])){
+                        result.add(str1[i]);
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        String[] commomStr = getCommonSubstrings(string1, string2);
+        int maxLength = 0;
+        String largest = "";
+
+        for (String s : commomStr) {
+            if (s.length() > maxLength) {
+                maxLength = s.length();
+                largest = s;
+            }
+        }
+
+        return largest;
     }
 }
